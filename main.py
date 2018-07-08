21d3658056108221f4d73ca82a973985f6871def
MrVestacus <33468702+MrVestacus@users.noreply.github.com>
2018-07-08 09:02:34 +0200

    lol

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from config import token
 from discord.ext import commands
 from discord.ext.commands import Bot
 
-token = os.environ["token"]
+token = os.environ["bot_token"]
 bot = commands.Bot(command_prefix="?")
 
 sloth_images = ["https://i.imgur.com/74ketp5.jpg", "https://i.imgur.com/27n3GOp.gif", "https://i.imgur.com/Y273oWN.gif",
